7fe936a18ac241526a132519f3c73a41e69a8831
yangshuai1208 <431317232@qq.com>
2026-05-16 18:33:32 +0800

    feat: add day11 tcp loop server demo

---
+++ day11_tcp_loop_server/tcp_loop_server.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SERVER_PORT 8888
+#define BACKLOG     5
+
+int main(void)
+{
+ int listenfd;
+ int connfd;
+ int ret;
+ int opt=1;
+
+ struct sockaddr_in server_addr;
+ struct sockaddr_in client_addr;
+ socklen_t client_len;
+ 
+ char recv_buf[128]={0};
+ 
+ listenfd=socket(AF_INET,SOCK_STREAM,0);
+ if(listenfd<0)
+ {
+  perror("socket failed");
+  return 1;
+ }
+ setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+
+ memset(&server_addr,0,sizeof(server_addr));
+
+ server_addr.sin_family=AF_INET;
+ server_addr.sin_port=htons(SERVER_PORT);
+ server_addr.sin_addr.s_addr=INADDR_ANY;
+ 
+ ret=bind(listenfd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+ if(ret<0)
+ {
+  perror("bind failed");
+  close(listenfd);
+  return 1;
+ }
+ ret=listen(listenfd,BACKLOG);
+ if(ret<0)
+ {
+   perror("listen failed");
+   close(listenfd);
+   return 1;
+ }
+ printf("tcp loop server listening on port:%d...\n",SERVER_PORT);
+ 
+ while(1)
+ {
+  client_len=sizeof(client_addr);
+  connfd=accept(listenfd,(struct sockaddr*)&client_addr,&client_len);
+  if(connfd<0)
+  {
+   perror("accept failed");
+   continue;
+  }
+  printf("client connected\n");
+  
+ while(1)
+  {
+    memset(recv_buf,0,sizeof(recv_buf));
+    ret=recv(connfd,recv_buf,sizeof(recv_buf)-1,0);
+    if(ret>0) 
+    {
+      printf("recv:%s",recv_buf);
+      send(connfd,recv_buf,strlen(recv_buf),0);
+    }
+    else if(ret==0)
+   {
+     printf("client disconnected\n");
+     break;
+   }
+   else 
+   {
+    perror("recv failed");
+    break;
+  }
+}
+}
+close(listenfd);
+
+return 0;
+}
